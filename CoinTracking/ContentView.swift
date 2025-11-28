8f803b184ee78aeadeddf85847bab352baeabcb3
IrisLei2808 <duclt2808@gmail.com>
2025-11-27 21:02:07 -0800

    test

--- CoinTracking/ContentView.swift
+++ CoinTracking/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, chrismaster!")
+            Text("Hello, ok!")
         }
         .padding()
     }
