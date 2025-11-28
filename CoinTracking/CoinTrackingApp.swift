8819f1b49df5a97909b7adbb0363295d64efab3d
IrisLei2808 <duclt2808@gmail.com>
2025-11-27 20:37:27 -0800

    Initial Commit

---
+++ CoinTracking/CoinTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTrackingApp.swift
+//  CoinTracking
+//
+//  Created by Owner on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct CoinTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
